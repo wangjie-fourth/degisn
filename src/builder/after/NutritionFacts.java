53d9da01390b5a785029f94dfcf0693937e9afcc
wangjie_fourth <wangjie_fourth@163.com>
2020-04-10 10:24:34 +0800

    [add](Builder)：添加创建者模式的实现

---
+++ src/builder/after/NutritionFacts.java
@@ -0,0 +1,71 @@
+package builder.after;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @ClassName NutritionFacts
+ * @Description
+ * @Author 25127
+ * @Date 2020/4/10 9:36
+ * @Email jie.wang13@hand-china.com
+ **/
+@Getter
+@Setter
+public class NutritionFacts {
+    private final int servingSize;
+    private final int serving;
+    private final int calories;
+    private final int fat;
+    private final int sodium;
+    private final int carbohydrate;
+
+    public static class Builder{
+        // must required
+        private final int servingSize;
+        private final int serving;
+        // optional parameter 、initialized to default value
+        private int calories = 0;
+        private int fat = 0;
+        private int sodium = 0;
+        private int carbohydrate = 0;
+
+        public Builder(int servingSize, int serving) {
+            this.servingSize = servingSize;
+            this.serving = serving;
+        }
+
+        public Builder withCalories(int calories) {
+            this.calories = calories;
+            return this;
+        }
+
+        public Builder withFat(int fat) {
+            this.fat = fat;
+            return this;
+        }
+
+        public Builder withSodium(int sodium) {
+            this.sodium = sodium;
+            return this;
+        }
+
+        public Builder withCarbohydrate(int carbohydrate) {
+            this.carbohydrate = carbohydrate;
+            return this;
+        }
+
+        public NutritionFacts build(){
+            return new NutritionFacts(this);
+        }
+    }
+
+    private NutritionFacts(Builder builder) {
+        this.servingSize = builder.servingSize;
+        this.serving = builder.serving;
+        this.calories = builder.calories;
+        this.fat = builder.fat;
+        this.sodium = builder.sodium;
+        this.carbohydrate = builder.carbohydrate;
+    }
+}
---
+++ src/builder/before/NutritionFacts.java
@@ -0,0 +1,42 @@
+package builder.before;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @ClassName NutritionFacts
+ * @Description
+ * @Author 25127
+ * @Date 2020/4/10 9:31
+ * @Email jie.wang13@hand-china.com
+ **/
+@Getter
+@Setter
+public class NutritionFacts {
+    private int servingSize;
+    private int serving;
+    private int calories;
+    private int fat;
+    private int sodium;
+    private int carbohydrate;
+
+    public NutritionFacts(int servingSize, int serving, int calories, int fat, int sodium, int carbohydrate) {
+        this.servingSize = servingSize;
+        this.serving = serving;
+        this.calories = calories;
+        this.fat = fat;
+        this.sodium = sodium;
+        this.carbohydrate = carbohydrate;
+    }
+
+    public NutritionFacts(int fat, int sodium) {
+        this.fat = fat;
+        this.sodium = sodium;
+    }
+
+    public NutritionFacts(int servingSize, int serving, int calories) {
+        this.servingSize = servingSize;
+        this.serving = serving;
+        this.calories = calories;
+    }
+}
